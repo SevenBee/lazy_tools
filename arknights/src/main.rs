ae24a0d6073432cad30343f3ef0139a411260f3f
SevenBee <945186289@qq.com>
2024-01-02 10:14:37 +0800

    ✨ feat: 添加对main函数的异步运行时

--- arknights/src/main.rs
+++ arknights/src/main.rs
@@ -5,7 +5,9 @@ use adb_helper::ADB;
 
 static GAME_PACKAGE_NAME: &'static str = "com.hypergryph.arknights";
 static GAME_LAUNCHABLE_ACTIVITY: &'static str = "com.u8.sdk.U8UnityContext";
-fn main() {
+
+#[tokio::main]
+async fn main() {
     let adb = ADB::new().init();
     let device = &adb.devices[0];
     adb.start_app(device, GAME_PACKAGE_NAME, GAME_LAUNCHABLE_ACTIVITY)
